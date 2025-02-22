3a726ca62007c94963ec0ce336bd76d20da88086
Abraham2403 <abrahamjesusper2006@gmail.com>
2025-02-22 14:23:59 -0400

    Ejercicio de abraham perozo

--- main.c
+++ main.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> //Aplicamos la Biblioteca
 
 int main()
 {
-    int arr[5] = {1, 2, 3, 4, 5};
-    int *p = arr;
+    int arr[5] = {1, 2, 3, 4, 5}; //Declaramos  e inicializamos las matrices
+    int *p = arr; //Aqui con el puntero p apuntamos la matriz
 
-    for (int i = 0; i < 5; i++) {
-        printf("Elemento %d: %d\n", i, *(p + i));
-        *(p + i) *= 2;
+    for (int i = 0; i < 5; i++) {                   //en esta parte con el bucle for se recorren los elementos de la matriz usando el puntero p
+        printf("Elemento %d: %d\n", i, *(p + i)); //aqui accede al elemento i de la matriz arrr
+        *(p + i) *= 2; //En esta parte se multiplico el valor del elemnto i por 2 i guarda el resultado de nuevo en la matriz.
     }
 
-    printf("Matriz modificada:\n");
+    printf("Matriz modificada:\n"); //aqui se imprime los elementos modificados de la matriz
     for (int i = 0; i < 5; i++) {
-        printf("Elemento %d: %d\n", i, arr[i]);
+        printf("Elemento %d: %d\n", i, arr[i]);//se imprime cada elemento junto con su indice
     }
-
     return 0;
 }
